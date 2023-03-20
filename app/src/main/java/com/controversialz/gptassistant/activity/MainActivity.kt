778ee78097f6a7df5f7bc6d3d97526f805565b01
Tulitu Vlad <tulituvlad@gmail.com>
2023-03-20 15:47:01 +0200

    Minor updates

--- app/src/main/java/com/controversialz/gptassistant/activity/MainActivity.kt
+++ app/src/main/java/com/controversialz/gptassistant/activity/MainActivity.kt
@@ -2,11 +2,27 @@ package com.controversialz.gptassistant.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatDelegate
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.controversialz.gptassistant.R
+import com.controversialz.gptassistant.databinding.ActivityMainBinding
+import com.controversialz.gptassistant.databinding.FragmentChatBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by viewBinding(ActivityMainBinding::bind)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        keepScreenAwake()
+        forceDayMode()
+    }
+
+    private fun forceDayMode() {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
+
+    private fun keepScreenAwake() {
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
     }
 }
